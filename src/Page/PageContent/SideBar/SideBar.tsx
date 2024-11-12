a39a03d8b56aacce3f05fc08177caaf89aa61678
lllzhmio <13999806+lllzhmio@user.noreply.gitee.com>
2024-11-13 01:04:33 +0800

    fix: smaller

--- src/Page/PageContent/SideBar/SideBar.tsx
+++ src/Page/PageContent/SideBar/SideBar.tsx
@@ -4,30 +4,16 @@ import { ESSAYS, useEssay } from '@store';
 
 import './SideBar.css';
 
-const ESSAY_COUNT = ESSAYS.length;
-const ESSAY_PER_PAGE = 8;
-const MAX_PAGE = Math.floor(ESSAY_COUNT / ESSAY_PER_PAGE);
 
 function SideBar() {
   const loadEssay = useEssay(state => state.loadEssay);
   const essayIdx = useEssay(state => state.essayIdx);
 
-  const [page, setPage] = useState<number>(0);
+  const [page] = useState<number>(0);
 
-  function changePage(diff: number) {
-    const target = page + diff;
-    if (target >= 0 && target <= MAX_PAGE) {
-      setPage(target);
-    }
-  }
 
   return (
     <div className="SideBar">
-      <img
-        src="images/icons/arrow-down-s-line.svg"
-        className={`Arrow Arrow__up ${page === 0 ? 'Arrow__hide' : ''}`}
-        alt=""
-        onClick={() => changePage(-1)}></img>
       <div className="EssayList">
         <div className="EssayList--content" style={{ transform: `translateY(${-637 * page}px)` }}>
           {ESSAYS.map(({ id }, i) =>
@@ -42,11 +28,6 @@ function SideBar() {
           )}
         </div>
       </div>
-      <img
-        src="images/icons/arrow-down-s-line.svg"
-        className={`Arrow ${page >= MAX_PAGE ? 'Arrow__hide' : ''}`}
-        alt=""
-        onClick={() => changePage(1)}></img>
     </div>
   );
 }
